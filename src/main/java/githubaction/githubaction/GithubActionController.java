249f3ee82ec3d6b468d211b668a6d200aedcc5ce
sweun2 <sweun3@gmail.com>
2022-11-29 13:11:35 +0900

    11111

---
+++ src/main/java/githubaction/githubaction/GithubActionController.java
@@ -0,0 +1,15 @@
+package githubaction.githubaction;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GithubActionController {
+    @GetMapping("/test")
+    public String githubActionTest() throws Exception {
+        System.out.println("test");
+        return "github action test";
+    }
+}
---
+++ src/main/java/githubaction/githubaction/GithubactionApplication.java
@@ -0,0 +1,13 @@
+package githubaction.githubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubactionApplication.class, args);
+	}
+
+}
